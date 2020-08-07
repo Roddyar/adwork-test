25d7799e3d8daaecb289d21ee108fda832c0c151
Roddy Arana Ruiz <roddyaru@gmail.com>
2020-08-07 13:29:15 -0500

    Malformed Data Validation

---
+++ src/main/java/com/adwork/adworktest/rest/controller/CustomControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.adwork.adworktest.rest.controller;
+
+import com.adwork.adworktest.rest.controller.error.ApiError;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+
+}
--- src/main/java/com/adwork/adworktest/rest/controller/UserController.java
+++ src/main/java/com/adwork/adworktest/rest/controller/UserController.java
@@ -4,12 +4,12 @@ import com.adwork.adworktest.db.entities.Response;
 import com.adwork.adworktest.db.entities.User;
 import com.adwork.adworktest.db.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -18,7 +18,7 @@ public class UserController {
     private IUserService userService;
 
     @PostMapping(value = "/save")
-    public Response saveUser(@RequestBody User user) {
+    public Response saveUser(@Valid @RequestBody User user) {
         Response response = userService.saveOrUpdateUser(user);
         return response;
     }
